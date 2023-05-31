7bdbb96d827abcfbe8c6fbf0562cb355044d967e
Herman Clarence Flaviano III <clarenceflavi@yahoo.com>
2023-06-01 00:54:45 +0800

    Added Parent Form

---
+++ models/ParentForm.cs
@@ -0,0 +1,25 @@
+﻿using Med_Docs.src;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Med_Docs.models
+{
+    public partial class ParentForm : Form
+    {
+
+        //Fields
+        User user;
+
+        //Constructor
+        public ParentForm(User user)
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Normal;
+
+            this.user = user;
+        }
+    }
+}
---
+++ models/ParentForm.designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Med_Docs.models
+{
+    partial class ParentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSideBar = new System.Windows.Forms.Panel();
+            this.pnlButtonArea = new System.Windows.Forms.Panel();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pnlTopBar = new System.Windows.Forms.Panel();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlSideBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSideBar
+            // 
+            this.pnlSideBar.AutoScroll = true;
+            this.pnlSideBar.BackColor = System.Drawing.Color.White;
+            this.pnlSideBar.Controls.Add(this.pnlButtonArea);
+            this.pnlSideBar.Controls.Add(this.pnlLogo);
+            this.pnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlSideBar.Name = "pnlSideBar";
+            this.pnlSideBar.Size = new System.Drawing.Size(200, 769);
+            this.pnlSideBar.TabIndex = 0;
+            // 
+            // pnlButtonArea
+            // 
+            this.pnlButtonArea.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlButtonArea.BackColor = System.Drawing.Color.White;
+            this.pnlButtonArea.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlButtonArea.Location = new System.Drawing.Point(0, 70);
+            this.pnlButtonArea.Name = "pnlButtonArea";
+            this.pnlButtonArea.Size = new System.Drawing.Size(200, 45);
+            this.pnlButtonArea.TabIndex = 4;
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.BackColor = System.Drawing.Color.White;
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(200, 70);
+            this.pnlLogo.TabIndex = 4;
+            // 
+            // pnlTopBar
+            // 
+            this.pnlTopBar.BackColor = System.Drawing.Color.White;
+            this.pnlTopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTopBar.Location = new System.Drawing.Point(200, 0);
+            this.pnlTopBar.Name = "pnlTopBar";
+            this.pnlTopBar.Size = new System.Drawing.Size(1397, 70);
+            this.pnlTopBar.TabIndex = 1;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.AutoScroll = true;
+            this.pnlMain.BackColor = System.Drawing.Color.White;
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(200, 70);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1397, 699);
+            this.pnlMain.TabIndex = 2;
+            // 
+            // ParentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1597, 769);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlTopBar);
+            this.Controls.Add(this.pnlSideBar);
+            this.MaximumSize = new System.Drawing.Size(1613, 808);
+            this.MinimumSize = new System.Drawing.Size(1613, 808);
+            this.Name = "ParentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TechRx - EMR System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pnlSideBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSideBar;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.Panel pnlTopBar;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlButtonArea;
+    }
+}
+
